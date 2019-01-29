2c8d59f35573005a39772f30131ff30e19dcf5db
Sean <seanmarciniak@gmail.com>
2019-01-29 13:16:39 +1030

    Adding small doc

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package termi is designed to make processing argument strings from the command line
+// easier and enable nested commands within applications supporting their own different
+// flags.
+package termi
