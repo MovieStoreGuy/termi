59daa2d09a27a664ca054144b45bc8b8f9a624e5
Sean <seanmarciniak@gmail.com>
2019-01-29 16:05:39 +1030

    Fixing function declarations

--- flag_integer.go
+++ flag_integer.go
@@ -14,49 +14,49 @@ func NewInteger() Flag {
 	return &Integer{&basic{names: map[string]bool{}}}
 }
 
-func (n *Integer) IsFlag(name string) bool {
-	if n.basic == nil {
-		n.basic = new(basic)
+func (i *Integer) IsFlag(name string) bool {
+	if i.basic == nil {
+		i.basic = new(basic)
 	}
-	return n.isFlag(name)
+	return i.isFlag(name)
 }
 
-func (s *Integer) Set(value string) error {
-	if s.basic == nil {
-		s.basic = new(basic)
+func (i *Integer) Set(value string) error {
+	if i.basic == nil {
+		i.basic = new(basic)
 	}
-	if !s.value.IsValid() {
+	if !i.value.IsValid() {
 		return errors.New("unable to update value if value is not valid")
 	}
 	v, err := strconv.Atoi(value)
-	s.value.Set(reflect.ValueOf(v))
+	i.value.Set(reflect.ValueOf(v))
 	return err
 }
 
-func (n *Integer) SetValue(value interface{}) Flag {
-	if n.basic == nil {
-		n.basic = new(basic)
+func (i *Integer) SetValue(value interface{}) Flag {
+	if i.basic == nil {
+		i.basic = new(basic)
 	}
-	n.value = reflect.Indirect(reflect.ValueOf(value))
-	return n
+	i.value = reflect.Indirect(reflect.ValueOf(value))
+	return i
 }
 
-func (n *Integer) SetDescription(description string) Flag {
-	if n.basic == nil {
-		n.basic = new(basic)
+func (i *Integer) SetDescription(description string) Flag {
+	if i.basic == nil {
+		i.basic = new(basic)
 	}
-	n.description = description
-	return n
+	i.description = description
+	return i
 }
 
-func (n *Integer) SetName(name string) Flag {
-	if n.basic == nil {
-		n.basic = new(basic)
+func (i *Integer) SetName(name string) Flag {
+	if i.basic == nil {
+		i.basic = new(basic)
 	}
-	n.setName(name)
-	return n
+	i.setName(name)
+	return i
 }
 
-func (n *Integer) String() string {
-	return n.basic.String("string")
+func (i *Integer) String() string {
+	return i.basic.String("string")
 }
